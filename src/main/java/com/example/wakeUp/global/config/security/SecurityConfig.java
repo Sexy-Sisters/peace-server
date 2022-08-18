3c82f90f8afb9d3d68f588a5989a38fcc4869e31
leekyukin <leekujin14@gmail.com>
2022-08-18 14:33:18 +0900

    SET :: Security Config

---
+++ src/main/java/com/example/wakeUp/global/config/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.wakeUp.global.config.security;
+
+import com.example.wakeUp.domain.user.domain.type.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/user/**").permitAll()
+                .anyRequest().hasRole("USER")
+        ;
+    }
+}
