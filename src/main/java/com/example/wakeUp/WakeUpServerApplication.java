f91320857867abddce1adc6419872f9ccfec43ea
leekyukin <leekujin14@gmail.com>
2022-08-18 10:25:18 +0900

    SET :: init

---
+++ src/main/java/com/example/wakeUp/WakeUpServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wakeUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeUpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakeUpServerApplication.class, args);
+    }
+
+}
