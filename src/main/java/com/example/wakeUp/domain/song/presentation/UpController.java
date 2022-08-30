31bd838cd7f18e58677b00145cf4913072582069
leekyukin <leekujin14@gmail.com>
2022-08-30 12:07:05 +0900

    ADD :: Push up & Cancel up

---
+++ src/main/java/com/example/wakeUp/domain/song/presentation/UpController.java
@@ -0,0 +1,27 @@
+package com.example.wakeUp.domain.song.presentation;
+
+import com.example.wakeUp.domain.song.service.UpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/song/{id}/up")
+public class UpController {
+
+    private final UpService upService;
+
+    @PostMapping
+    public void pushUp(
+            @PathVariable Long id
+    ) {
+        upService.pushUp(id);
+    }
+
+    @DeleteMapping
+    public void deleteUp(
+            @PathVariable Long id
+    ) {
+        upService.cancelUp(id);
+    }
+}
---
+++ src/main/java/com/example/wakeUp/domain/song/service/UpService.java
@@ -0,0 +1,40 @@
+package com.example.wakeUp.domain.song.service;
+
+import com.example.wakeUp.domain.song.domain.Song;
+import com.example.wakeUp.domain.song.domain.Up;
+import com.example.wakeUp.domain.song.domain.repository.UpRepository;
+import com.example.wakeUp.domain.song.facade.SongFacade;
+import com.example.wakeUp.domain.song.service.facade.UpFacade;
+import com.example.wakeUp.domain.user.domain.User;
+import com.example.wakeUp.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UpService {
+
+    private final UpRepository upRepository;
+    private final UserFacade userFacade;
+    private final SongFacade songFacade;
+    private final UpFacade upFacade;
+
+    @Transactional
+    public void pushUp(Long id) {
+        User user = userFacade.findByEmail(userFacade.securityUtil());
+        Song song = songFacade.findSongById(id);
+        upFacade.validatePushUp(user, song);
+
+        upRepository.save(Up.createUp(user, song));
+    }
+
+    @Transactional
+    public void cancelUp(Long id) {
+        User user = userFacade.findByEmail(userFacade.securityUtil());
+        Song song = songFacade.findSongById(id);
+        upFacade.validateCancelUp(user, song);
+
+        upRepository.delete(upFacade.findUpByUserSong(user, song));
+    }
+}
