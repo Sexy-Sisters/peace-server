5cfd0f5e54e1ea6b89188a86a54a18e3aa752a0f
leekyukin <leekujin14@gmail.com>
2022-09-04 17:03:11 +0900

    ADD :: 비어있는 문자열 체크

--- src/main/java/com/example/wakeUp/domain/song/facade/SongFacade.java
+++ src/main/java/com/example/wakeUp/domain/song/facade/SongFacade.java
@@ -1,6 +1,7 @@
 package com.example.wakeUp.domain.song.facade;
 
 import com.example.wakeUp.domain.song.domain.Song;
+import com.example.wakeUp.domain.song.exception.EmptyStringException;
 import com.example.wakeUp.domain.song.exception.SongNotFoundException;
 import com.example.wakeUp.domain.song.presentation.dto.request.CreateSongRequestDto;
 import com.example.wakeUp.domain.song.domain.repository.SongRepository;
@@ -8,8 +9,10 @@ import com.example.wakeUp.domain.song.exception.SongAlreadyExistsException;
 import com.example.wakeUp.domain.song.exception.AlreadyRequestSongException;
 import com.example.wakeUp.domain.user.domain.User;
 import com.example.wakeUp.global.Utils.DateUtil;
+import io.netty.util.internal.StringUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.util.StringUtils;
 
 @Component
 @RequiredArgsConstructor
@@ -18,8 +21,9 @@ public class SongFacade {
     private final SongRepository songRepository;
 
     public void validateRequestSong(CreateSongRequestDto dto, User user) {
-
-        if (songRepository.existsByUserAndCreatedAtBetween(user, DateUtil.getToday(), DateUtil.getTomorrow()))
+        if (StringUtils.isEmpty(dto.getTitle()) && StringUtils.isEmpty(dto.getSinger()))
+            throw EmptyStringException.EXCEPTION;
+        else if (songRepository.existsByUserAndCreatedAtBetween(user, DateUtil.getToday(), DateUtil.getTomorrow()))
             throw AlreadyRequestSongException.EXCEPTION;
 
         else if (songRepository.existsByTitleAndSingerAndCreatedAtBetween(dto.getTitle(), dto.getSinger(), DateUtil.getToday(), DateUtil.getTomorrow()))
