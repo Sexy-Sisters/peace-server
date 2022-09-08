de39f7a6bcbea884e98892c92bf8b44476f878f5
leekyukin <leekujin14@gmail.com>
2022-09-08 10:59:21 +0900

    ADD :: Like 연관관계

---
+++ src/main/java/com/example/wakeUp/domain/chart/domain/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.wakeUp.domain.chart.domain.repository;
+
+import com.example.wakeUp.domain.chart.domain.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
