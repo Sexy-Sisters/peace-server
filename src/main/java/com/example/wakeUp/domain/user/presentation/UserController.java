1e5ed8c74642518b0014239018512490520fcd10
leekyukin <leekujin14@gmail.com>
2022-09-10 16:18:08 +0900

    ADD :: API Logging

--- src/main/java/com/example/wakeUp/domain/user/presentation/UserController.java
+++ src/main/java/com/example/wakeUp/domain/user/presentation/UserController.java
@@ -5,6 +5,7 @@ import com.example.wakeUp.domain.user.presentation.dto.request.CodeRequestDto;
 import com.example.wakeUp.domain.user.presentation.dto.request.CreateUserRequestDto;
 import com.example.wakeUp.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,34 +13,33 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userServiceImp;
     private final UserRepository userRepository;
 
     @PostMapping
-    public void signUp(
-            @RequestBody @Valid CreateUserRequestDto request
-    ) {
+    public void signUp(@RequestBody @Valid CreateUserRequestDto request) {
+        log.info("<<<<<====== [POST]: /api/user =====>>>>>");
         userServiceImp.signUp(request);
     }
 
     @GetMapping("/issue-code")
-    public void issueCode(
-            @RequestParam String email
-    ) {
-       userServiceImp.issueCode(email);
+    public void issueCode(@RequestParam String email) {
+        log.info("<<<<<====== [GET]: /api/user =====>>>>>");
+        userServiceImp.issueCode(email);
     }
 
     @PostMapping("/check-code")
-    public boolean checkCode(
-            @RequestBody CodeRequestDto request
-    ) {
+    public boolean checkCode(@RequestBody CodeRequestDto request) {
+        log.info("<<<<<====== [POST]: /api/user/check-code =====>>>>>");
         return userServiceImp.checkCode(request.getCode(), request.getEmail());
     }
 
     @DeleteMapping
     public void deleteAll() {
+        log.info("<<<<<====== [DELETE]: /api/user =====>>>>>");
         userRepository.deleteAll();
     }
 }
