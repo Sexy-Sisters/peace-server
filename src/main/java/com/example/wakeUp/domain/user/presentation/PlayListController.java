422574c42e61753f677455e3e16c16563f0f76e3
leekyukin <leekujin14@gmail.com>
2022-09-21 16:46:00 +0900

    FEAT :: 플레이 리스트 조회

--- src/main/java/com/example/wakeUp/domain/user/presentation/PlayListController.java
+++ src/main/java/com/example/wakeUp/domain/user/presentation/PlayListController.java
@@ -1,11 +1,13 @@
 package com.example.wakeUp.domain.user.presentation;
 
 import com.example.wakeUp.domain.user.presentation.dto.request.AddPlayListRequestDto;
+import com.example.wakeUp.domain.user.presentation.dto.response.PlayListResponseDto;
 import com.example.wakeUp.domain.user.service.PlayListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/playlist")
@@ -19,8 +21,14 @@ public class PlayListController {
         playListService.addPlayList(dto);
     }
 
+    @GetMapping("/{userId}")
+    public List<PlayListResponseDto> findPlayList(@PathVariable Long userId) {
+        return playListService.findPlayList(userId);
+    }
+
     @DeleteMapping("/{id}")
     public void deletePlayList(@PathVariable Long id) {
         playListService.deletePlayList(id);
     }
+
 }
