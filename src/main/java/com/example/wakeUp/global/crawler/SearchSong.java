e383962b6c7fdadd0cf7018d19133c6cb7487218
leekyukin <leekujin14@gmail.com>
2022-10-11 12:30:26 +0900

    ADD :: prefix 삭제

--- src/main/java/com/example/wakeUp/global/crawler/SearchSong.java
+++ src/main/java/com/example/wakeUp/global/crawler/SearchSong.java
@@ -3,7 +3,6 @@ package com.example.wakeUp.global.crawler;
 import com.example.wakeUp.global.crawler.dto.SearchSongDto;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
@@ -27,18 +26,29 @@ public class SearchSong {
         Elements trList = doc.select("#body-content > div.search_song > div.search_result_detail > div > table > tbody > tr");
 
         List<SearchSongDto> searchSongList = new ArrayList<>();
-        for ( Element tr : trList ) {
-            String imgUrl = tr.select("td > a > img").attr("src");
-            String title = tr.select("td.info > a.title.ellipsis").text();
-            String singer = tr.select("td.info > a.artist.ellipsis").text();
+        int size = Math.min(trList.size(), 10);
+        if (!trList.isEmpty()) {
+            for ( int i = 0; i < size; i++ ) {
+                String imgUrl = trList.get(i).select("td > a > img").attr("src");
+                String title = trList.get(i).select("td.info > a.title.ellipsis").text();
+                String singer = trList.get(i).select("td.info > a.artist.ellipsis").text();
 
-            SearchSongDto searchSongDto = SearchSongDto.builder()
-                    .imgUrl(imgUrl)
-                    .title(title)
-                    .singer(singer)
-                    .build();
+                if (title.startsWith("TITLE ")) {
+                    title = title.substring(6, title.length());
+                }
+                if (title.startsWith("19금 ")) {
+                    title = title.substring(4, title.length());
+                }
 
-            searchSongList.add(searchSongDto);
+
+                SearchSongDto searchSongDto = SearchSongDto.builder()
+                        .imgUrl(imgUrl)
+                        .title(title)
+                        .singer(singer)
+                        .build();
+
+                searchSongList.add(searchSongDto);
+            }
         }
 
         return searchSongList;
