475cf834ccbcfc4f23472842dd5beca1d215e7a6
leekyukin <leekujin14@gmail.com>
2022-10-11 11:05:56 +0900

    FIX :: 음원사이트 변경

--- src/main/java/com/example/wakeUp/global/crawler/SearchSong.java
+++ src/main/java/com/example/wakeUp/global/crawler/SearchSong.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class SearchSong {
 
     public List<SearchSongDto> search(String word) {
-        String url = "https://music.bugs.co.kr/search/integrated?q=" + word;
+        String url = "https://www.genie.co.kr/search/searchMain?query=" + word;
 
         Document doc = null;
         try {
@@ -24,13 +24,13 @@ public class SearchSong {
             e.printStackTrace();
         }
 
-        Elements trList = doc.select("#DEFAULT0 > table > tbody > tr");
+        Elements trList = doc.select("#body-content > div.search_song > div.search_result_detail > div > table > tbody > tr");
 
         List<SearchSongDto> searchSongList = new ArrayList<>();
         for ( Element tr : trList ) {
-            String imgUrl = tr.select("a > img").attr("src");
-            String title = tr.select("th > p").text();
-            String singer = tr.select("td > p").text();
+            String imgUrl = tr.select("td > a > img").attr("src");
+            String title = tr.select("td.info > a.title.ellipsis").text();
+            String singer = tr.select("td.info > a.artist.ellipsis").text();
 
             SearchSongDto searchSongDto = SearchSongDto.builder()
                     .imgUrl(imgUrl)
